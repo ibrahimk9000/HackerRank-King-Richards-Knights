2de474fb18ab854b8771208756027fc95a2bacc6
ibrahim dev <ibra1990ski@gmail.com>
2019-06-29 21:48:35 +0200

    some hange

--- main.go
+++ main.go
@@ -16,10 +16,12 @@ import (
  * Complete the kingRichardKnights function below.
  */
 type abn struct {
-	a   int32
-	b   int32
-	n   int32
-	ran int
+	a     uint64
+	b     uint64
+	n     uint64
+	ran   int
+	angle int
+	com   int
 }
 type sortstruct []abn
 
@@ -27,123 +29,140 @@ func (a sortstruct) Len() int           { return len(a) }
 func (a sortstruct) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 func (a sortstruct) Less(i, j int) bool { return a[i].ran < a[j].ran }
 
-func tranf(dis, ang, ring, x, y int32) (int32, int32) {
-	var a, b int32
-	switch ang {
+func tranf(k abn, com []uint64) (uint64, uint64) {
+	var a, b uint64
+	switch k.angle {
+	case 0:
+		a = com[0] + k.a - 1
+		b = com[1] + k.b - 1
+		break
 	case 1:
-		a = x + dis
-		b = y + ring
+
+		a = com[2] + com[1] - k.a + 1
+		b = com[0] + k.b - 1
 		break
 	case 2:
-		a = x + ring
-		b = y + ring - dis
-		break
-	case 3:
-		a = x + ring - dis
-		b = y
+		a = com[2] + com[0] - k.a + 1
+		b = com[2] + com[1] - k.b + 1
 		break
 
-	case 4:
-		a = x
-		b = y + dis
+	case 3:
+		a = com[1] + k.a - 1
+		b = com[2] + com[0] - k.b + 1
 		break
 	}
 	return a, b
 }
-func analyse(k abn, al []int32) (int32, int32, int32, int32, int32) {
-	var dis, ang, ring int32
-	var v = make([]int32, 3)
-	copy(v, al)
-	s := v[2] + 1
+func analyse(k abn, x, y, ksx, ksy, com uint64, xi int) abn {
+	//	var dis, ang, ring uint64
 
-	for i := int32(0); i < s/2; i++ {
-		if k.a == v[0] && k.b <= v[1]+v[2] {
-			dis = k.b - v[1]
-			ang = 1
-			ring = v[2]
-			break
-		}
-		if k.a <= v[0]+v[2] && k.b == v[1]+v[2] {
-			dis = k.a - v[0]
-			ang = 2
-			ring = v[2]
-			break
-		}
-		if k.a == v[0]+v[2] && k.b >= v[1] {
-			dis = v[1] + v[2] - k.b
-			ang = 3
-			ring = v[2]
-			break
-		}
-		if k.a >= v[0] && k.b == v[1] {
-			dis = v[0] + v[2] - k.a
-			ang = 4
-			ring = v[2]
-			break
-		}
-		//fmt.Println("ring ", k, v)
-		v[0]++
-		v[1]++
-		v[2] -= 2
+	switch k.angle {
+	case 0:
+		k.a -= x
+		k.b -= y
+
+	case 1:
+		k.a -= ksy
+		k.b -= x
+	case 2:
+		k.a -= ksx
+		k.b -= ksy
+	case 3:
+		k.a -= y
+		k.b -= ksx
+
 	}
-
-	return dis, ang, ring, v[0], v[1]
+	//	println(k.a, k.b, com)
+	if k.a == 0 || k.b == 0 || k.a > com+1 || k.b > com+1 {
+		return abn{0, 0, 0, 0, 0, 0}
+	}
+	k.angle = (k.angle + 1) % 4
+	k.com++
+	return k
 }
-func kingRichardKnights(n int32, s int32, knights []int32, com [][]int32) [][]int32 {
+func kingRichardKnights(n uint64, s uint64, knights []uint64, com [][]uint64) [][]uint64 {
 
 	pos := make([]abn, len(knights))
-	result := make([][]int32, len(knights))
+	result := make([][]uint64, len(knights))
+	incom := []uint64{1, 1, n - 1}
+	var x, y, ksx, ksy uint64
+
 	guid := 0
 	for i := 0; i < len(knights); i++ {
 		pos[i].a = (knights[i] / n) + 1
 		pos[i].b = (knights[i] % n) + 1
 		pos[i].n = knights[i]
 		pos[i].ran = i
+		pos[i].com = -1
+		pos[i].angle = 0
+
 		//	fmt.Fprintf(os.Stdout, "result %d \n", pos[i].ran)
 	}
-	for _, v := range com {
+
+	for ix, v := range com {
+		if ix != 0 {
+			x = v[0] - com[ix-1][0]
+			y = v[1] - com[ix-1][1]
+			ksx = com[ix-1][2] - v[2] - x
+			ksy = com[ix-1][2] - v[2] - y
+		} else {
+			x = v[0] - incom[0]
+			y = v[1] - incom[1]
+			ksx = incom[2] - v[2] - x
+			ksy = incom[2] - v[2] - y
+		}
+		if ix%1000 == 0 {
+			log.Println(ix, "   thousand    ",guid,"offset")
+		}
 		for ii := guid; ii < len(pos); ii++ {
-			if v[2] > 0 {
-				start := time.Now()
-				if (pos[ii].a >= v[0] && pos[ii].a <= v[0]+v[2]) && (pos[ii].b >= v[1] && pos[ii].b <= v[1]+v[2]) {
-					dis, ang, ring, vx, vy := analyse(pos[ii], v)
-					//fmt.Println("ang", dis, ang, ring)
-					//	fmt.Println("forw", pos[ii])
-					//	fmt.Println("changed", v)
+			//	if v[2] > 0 {
 
-					a, b := tranf(dis, ang, ring, vx, vy)
-					//fmt.Println("arg", a, b)
-					pos[ii].a = a
-					pos[ii].b = b
-					elapsed := time.Since(start)
-					log.Printf("%d time elapse %s", guid, elapsed)
-					///	fmt.Fprintf(os.Stdout, "result %d \n", pos[ii].ran)
-					//	fmt.Fprintf(os.Stdout, "resul%d , %d , %d\n", pos[ii].a, pos[ii].b, pos[ii].ran)
-				} else {
-					//fmt.Fprintf(os.Stdout, "%d , %d \n", guid, ii)
-					start := time.Now()
-					sortstruct(pos).Swap(guid, ii)
-					elapsed := time.Since(start)
-					log.Printf("Binomial took %s", elapsed)
-					guid++
+			//if (pos[ii].a >= 0 && pos[ii].a <= v[2]) && (pos[ii].b >= 0 && pos[ii].b <= v[2]) {
 
-				}
+			sd := analyse(pos[ii], x, y, ksx, ksy, v[2], ix)
+			//	fmt.Println(pos[ii])
+			if sd.a != 0 {
+				pos[ii] = sd
 
-				guid = 0
+				//a, b := tranf(di					s, ang, ring, vx, vy)
+				//
+				//fmt.Println(pos)
+				//fmt.Println(x, y, ksx, ksy)
+				//pos[ii].a = a
+				//pos[ii].b = b
+
+			} else {
+
+				sortstruct(pos).Swap(guid, ii)
+
+				guid++
+
 			}
 		}
+
+		//guid = 0
+
+		//	}
 	}
 	sort.Sort(sortstruct(pos))
 	for i, v := range pos {
-		result[i] = append(result[i], v.a)
-		result[i] = append(result[i], v.b)
+		var a, b uint64
+
+		if v.com == -1 {
+			a, b = tranf(v, incom)
+		} else {
+			a, b = tranf(v, com[v.com])
+		}
+		//	fmt.Println(pos)
+		result[i] = append(result[i], a)
+		result[i] = append(result[i], b)
 		//fmt.Fprintf(os.Stdout, "result %d , %d , %d\n", v.a, v.b, v.ran)
 	}
 	return result
 }
 
 func main() {
-	var knights []int32
+	var knights []uint64
 	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
 
 	//stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
@@ -155,21 +174,21 @@ func main() {
 
 	nTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
 	checkError(err)
-	n := int32(nTemp)
+	n := uint64(nTemp)
 
 	sTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
 	checkError(err)
-	s := int32(sTemp)
+	s := uint64(sTemp)
 
-	var commands [][]int32
+	var commands [][]uint64
 	for commandsRowItr := 0; commandsRowItr < int(s); commandsRowItr++ {
 		commandsRowTemp := strings.Split(readLine(reader), " ")
 
-		var commandsRow []int32
+		var commandsRow []uint64
 		for _, commandsRowItem := range commandsRowTemp {
 			commandsItemTemp, err := strconv.ParseInt(commandsRowItem, 10, 64)
 			checkError(err)
-			commandsItem := int32(commandsItemTemp)
+			commandsItem := uint64(commandsItemTemp)
 			commandsRow = append(commandsRow, commandsItem)
 		}
 
@@ -181,16 +200,22 @@ func main() {
 	}
 	vTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
 	checkError(err)
-	v := int32(vTemp)
-	for i := 0; i < int(v); i++ {
+	v := uint64(vTemp)
+	for i := uint64(0); i < v; i++ {
 
 		knightstemp, err := strconv.ParseInt(readLine(reader), 10, 64)
 		checkError(err)
 
-		knights = append(knights, int32(knightstemp))
+		knights = append(knights, uint64(knightstemp))
 
 	}
+	//
+	start := time.Now()
 	result := kingRichardKnights(n, s, knights, commands)
+
+	elapsed := time.Since(start)
+	log.Printf("Binomial took %s", elapsed.String())
+	//
 
 	for resultRowItr, rowItem := range result {
 		for resultColumnItr, colItem := range rowItem {
