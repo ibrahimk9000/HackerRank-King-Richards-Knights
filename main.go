4ce178e3319046c294bd4b1c0783b344aa90cd62
ibrahim dev <ibra1990ski@gmail.com>
2019-07-02 17:44:09 +0200

    for github

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"io"
 	"log"
 	"os"
-	"sort"
 	"strconv"
 	"strings"
 	"time"
@@ -16,18 +15,13 @@ import (
  * Complete the kingRichardKnights function below.
  */
 type abn struct {
-	a     uint64
-	b     uint64
-	n     uint64
-	ran   int
+	a uint64
+	b uint64
+	//n     uint64
+	//ran   int
 	angle int
 	com   int
 }
-type sortstruct []abn
-
-func (a sortstruct) Len() int           { return len(a) }
-func (a sortstruct) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a sortstruct) Less(i, j int) bool { return a[i].ran < a[j].ran }
 
 func tranf(k abn, com []uint64) (uint64, uint64) {
 	var a, b uint64
@@ -53,110 +47,128 @@ func tranf(k abn, com []uint64) (uint64, uint64) {
 	}
 	return a, b
 }
-func analyse(k abn, x, y, ksx, ksy, com uint64, xi int) abn {
+func analyse(k abn, s []uint64, i, u int, com uint64) (uint64, uint64) {
 	//	var dis, ang, ring uint64
+	k.a -= s[i]
+	k.b -= s[u]
 
-	switch k.angle {
-	case 0:
-		k.a -= x
-		k.b -= y
-
-	case 1:
-		k.a -= ksy
-		k.b -= x
-	case 2:
-		k.a -= ksx
-		k.b -= ksy
-	case 3:
-		k.a -= y
-		k.b -= ksx
-
-	}
 	//	println(k.a, k.b, com)
-	if k.a == 0 || k.b == 0 || k.a > com+1 || k.b > com+1 {
-		return abn{0, 0, 0, 0, 0, 0}
+	if k.a <= 0 || k.b <= 0 || k.a > com+1 || k.b > com+1 {
+		return 0, 0
 	}
-	k.angle = (k.angle + 1) % 4
-	k.com++
-	return k
+
+	return k.a, k.b
 }
 func kingRichardKnights(n uint64, s uint64, knights []uint64, com [][]uint64) [][]uint64 {
 
 	pos := make([]abn, len(knights))
 	result := make([][]uint64, len(knights))
+	tcom := make([][]uint64, len(com))
 	incom := []uint64{1, 1, n - 1}
-	var x, y, ksx, ksy uint64
-
-	guid := 0
+	//ind := [][]int{{0, 1}, {3, 0}, {2, 3}, {1, 2}}
+	//angle := 0
 	for i := 0; i < len(knights); i++ {
 		pos[i].a = (knights[i] / n) + 1
 		pos[i].b = (knights[i] % n) + 1
-		pos[i].n = knights[i]
-		pos[i].ran = i
+		//pos[i].n = knights[i]
+		//pos[i].ran = i
 		pos[i].com = -1
 		pos[i].angle = 0
 
 		//	fmt.Fprintf(os.Stdout, "result %d \n", pos[i].ran)
 	}
 
-	for ix, v := range com {
-		if ix != 0 {
-			x = v[0] - com[ix-1][0]
-			y = v[1] - com[ix-1][1]
-			ksx = com[ix-1][2] - v[2] - x
-			ksy = com[ix-1][2] - v[2] - y
+	for i := 0; i < len(com); i++ {
+		var x, y, ksx, ksy uint64
+		if i != 0 {
+			x = com[i][0] - com[i-1][0]
+			y = com[i][1] - com[i-1][1]
+			ksx = com[i-1][2] - com[i][2] - x
+			ksy = com[i-1][2] - com[i][2] - y
 		} else {
-			x = v[0] - incom[0]
-			y = v[1] - incom[1]
-			ksx = incom[2] - v[2] - x
-			ksy = incom[2] - v[2] - y
+			x = com[i][0] - incom[0]
+			y = com[i][1] - incom[1]
+			ksx = incom[2] - com[i][2] - x
+			ksy = incom[2] - com[i][2] - y
 		}
-		if ix%1000 == 0 {
-			log.Println(ix, "   thousand    ",guid,"offset")
-		}
-		for ii := guid; ii < len(pos); ii++ {
-			//	if v[2] > 0 {
+		f := []uint64{x, y, ksx, ksy}
 
-			//if (pos[ii].a >= 0 && pos[ii].a <= v[2]) && (pos[ii].b >= 0 && pos[ii].b <= v[2]) {
+		tcom[i] = append(tcom[i], f...)
 
-			sd := analyse(pos[ii], x, y, ksx, ksy, v[2], ix)
-			//	fmt.Println(pos[ii])
-			if sd.a != 0 {
-				pos[ii] = sd
+	}
+	//	sk := time.Now()
+	//ee := time.Since(sk)
+	//ee = 0
+	for ii := 0; ii < len(tcom); ii++ {
 
-				//a, b := tranf(di					s, ang, ring, vx, vy)
-				//
-				//fmt.Println(pos)
-				//fmt.Println(x, y, ksx, ksy)
-				//pos[ii].a = a
-				//pos[ii].b = b
+		//	angle = 0
+		fmt.Println(tcom[ii])
+		/*
+				for i, v := range tcom {
 
-			} else {
+					//
 
-				sortstruct(pos).Swap(guid, ii)
+					//	if v[2] > 0 {com
 
-				guid++
+					//if (pos[ii].a >= 0 && pos[ii].a <= v[2]) && (pos[ii].b >= 0 && pos[ii].b <= v[2]) {
+					start := time.Now()
+
+					sd, sb := analyse(pos[ii], v, ind[angle][0], ind[angle][1], com[i][2])
+					elapsed := time.Since(start)
+					ee += elapsed
+					if ii%1000 == 0 {
+						log.Println(" Binomial took ", ee.String())
+					}
+					//	fmt.Println(pos[ii]
+					if sd != 0 && len(com)-i > 1 {
+
+						pos[ii].a = sd
+						pos[ii].b = sb
+						angle = (angle + 1) % 4
+						pos[ii].com++
+						continue
+
+						//a, b := tranf(di					s, ang, ring, vx, vy)
+						//
+						//fmt.Println(pos)
+						//fmt.Println(x, y, ksx, ksy)
+						//pos[ii].a = a
+						//pos[ii].b = b
+
+					}
+					if len(com)-i == 1 {
+						pos[ii].a = sd
+						pos[ii].b = sb
+						angle = (angle + 1) % 4
+
+						pos[ii].com++
+					}
+					var a, b uint64
+					pos[ii].angle = angle
+					if pos[ii].com == -1 {
+						a, b = tranf(pos[ii], incom)
+					} else {
+						a, b = tranf(pos[ii], com[pos[ii].com])
+					}
+					//	fmt.Println(pos)
+
+					result[ii] = append(result[ii], a)
+					result[ii] = append(result[ii], b)
+					break
+					//sortstruct(pos).Swap(guid, ii)
+
+				}
 
 			}
-		}
+			//	}
 
-		//guid = 0
+			//	sort.Sort(sortstruct(pos))
+			//	for = range pos {
+			//		 uint64
 
-		//	}
-	}
-	sort.Sort(sortstruct(pos))
-	for i, v := range pos {
-		var a, b uint64
-
-		if v.com == -1 {
-			a, b = tranf(v, incom)
-		} else {
-			a, b = tranf(v, com[v.com])
-		}
-		//	fmt.Println(pos)
-		result[i] = append(result[i], a)
-		result[i] = append(result[i], b)
-		//fmt.Fprintf(os.Stdout, "result %d , %d , %d\n", v.a, v.b, v.ran)
+			//fmt.Fprintf(os.Stdout, "result %d , %d , %d\n", v.a, v.b, v.ran)
+		*/
+		//}
 	}
 	return result
 }
