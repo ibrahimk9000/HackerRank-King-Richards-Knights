19020744e301d960b25aa3c8b5c42946f85dfabc
ibrahim dev <ibra1990ski@gmail.com>
2019-07-05 14:56:27 +0200

    reform

--- main.go
+++ main.go
@@ -47,24 +47,27 @@ func tranf(k abn, com []uint64) (uint64, uint64) {
 	}
 	return a, b
 }
-func analyse(k abn, s []uint64, i, u int, com uint64) (uint64, uint64) {
+func analyse(k abn, com []uint64, s int, c uint64) (bool, uint64, uint64) {
 	//	var dis, ang, ring uint64
-	k.a -= s[i]
-	k.b -= s[u]
+	//k.a -= s[i]
+	//k.b -= s[u]
+	var a, b uint64
+	a = k.a - com[0]
+	b = k.b - com[1]
 
-	//	println(k.a, k.b, com)
-	if k.a <= 0 || k.b <= 0 || k.a > com+1 || k.b > com+1 {
-		return 0, 0
+	//	fmt.Println("qes ", a, b)
+	if a <= 0 || b <= 0 || a > c+1 || b > c+1 {
+		return true, a, b
 	}
 
-	return k.a, k.b
+	return false, a, b
 }
 func kingRichardKnights(n uint64, s uint64, knights []uint64, com [][]uint64) [][]uint64 {
 
 	pos := make([]abn, len(knights))
 	result := make([][]uint64, len(knights))
 	tcom := make([][]uint64, len(com))
-	incom := []uint64{1, 1, n - 1}
+	incom := []uint64{0, 0}
 	//ind := [][]int{{0, 1}, {3, 0}, {2, 3}, {1, 2}}
 	//angle := 0
 	for i := 0; i < len(knights); i++ {
@@ -79,97 +82,132 @@ func kingRichardKnights(n uint64, s uint64, knights []uint64, com [][]uint64) []
 	}
 
 	for i := 0; i < len(com); i++ {
-		var x, y, ksx, ksy uint64
+		var x, y, ksx, ksy, res1, res2 uint64
+		//	f := []uint64{x, y, ksx, ksy}
 		if i != 0 {
 			x = com[i][0] - com[i-1][0]
 			y = com[i][1] - com[i-1][1]
 			ksx = com[i-1][2] - com[i][2] - x
-			ksy = com[i-1][2] - com[i][2] - y
+			ksy = com[i-1][2] - com[i][2] - y //com[i][2] - y
+			switch i % 4 {
+			case 0:
+				res1 = tcom[i-1][0] + x
+				res2 = tcom[i-1][1] + y
+			case 1:
+				res1 = tcom[i-1][0] + ksy
+				res2 = tcom[i-1][1] + x
+			case 2:
+				res1 = tcom[i-1][0] + ksx
+				res2 = tcom[i-1][1] + ksy
+			case 3:
+				res1 = tcom[i-1][0] + y
+				res2 = tcom[i-1][1] + ksx
+			}
 		} else {
-			x = com[i][0] - incom[0]
-			y = com[i][1] - incom[1]
-			ksx = incom[2] - com[i][2] - x
-			ksy = incom[2] - com[i][2] - y
+			x = com[i][0] - 1
+			y = com[i][1] - 1
+			ksx = n - com[i][2] - x
+			ksy = n - com[i][2] - y
+			//	f = []uint64{x, y, ksx, ksy}
+			res1 = x
+			res2 = y
 		}
-		f := []uint64{x, y, ksx, ksy}
-
-		tcom[i] = append(tcom[i], f...)
+		tcom[i] = append(tcom[i], res1)
+		tcom[i] = append(tcom[i], res2)
 
 	}
 	//	sk := time.Now()
 	//ee := time.Since(sk)
 	//ee = 0
-	for ii := 0; ii < len(tcom); ii++ {
+	for ii := 0; ii < len(pos); ii++ {
+		//fmt.Println(tcom)
+		//angle =
+		gr := len(com) - 1
+		sm := 0
+		sgg := 0
+		//	var dc, cc uint64
+		fmt.Println(tcom)
+		//var kf, gf uint64
+		for g := 0; g < 4; g++ {
+			//	start := time.Now()
+			ss := (gr + sm) / 2
+			sd, _, _ := analyse(pos[ii], tcom[ss], ss, com[ss][2])
+			//fmt.Println(ii, sd, ss)
+			//	sd := true
+			if sd == true {
 
-		//	angle = 0
-		fmt.Println(tcom[ii])
-		/*
-				for i, v := range tcom {
+				//fmt.Println(kf, gf)
+				gr = ss
+				sgg = ss
 
-					//
-
-					//	if v[2] > 0 {com
-
-					//if (pos[ii].a >= 0 && pos[ii].a <= v[2]) && (pos[ii].b >= 0 && pos[ii].b <= v[2]) {
-					start := time.Now()
-
-					sd, sb := analyse(pos[ii], v, ind[angle][0], ind[angle][1], com[i][2])
-					elapsed := time.Since(start)
-					ee += elapsed
-					if ii%1000 == 0 {
-						log.Println(" Binomial took ", ee.String())
-					}
-					//	fmt.Println(pos[ii]
-					if sd != 0 && len(com)-i > 1 {
-
-						pos[ii].a = sd
-						pos[ii].b = sb
-						angle = (angle + 1) % 4
-						pos[ii].com++
-						continue
-
-						//a, b := tranf(di					s, ang, ring, vx, vy)
-						//
-						//fmt.Println(pos)
-						//fmt.Println(x, y, ksx, ksy)
-						//pos[ii].a = a
-						//pos[ii].b = b
-
-					}
-					if len(com)-i == 1 {
-						pos[ii].a = sd
-						pos[ii].b = sb
-						angle = (angle + 1) % 4
-
-						pos[ii].com++
-					}
-					var a, b uint64
-					pos[ii].angle = angle
-					if pos[ii].com == -1 {
-						a, b = tranf(pos[ii], incom)
-					} else {
-						a, b = tranf(pos[ii], com[pos[ii].com])
-					}
-					//	fmt.Println(pos)
-
-					result[ii] = append(result[ii], a)
-					result[ii] = append(result[ii], b)
-					break
-					//sortstruct(pos).Swap(guid, ii)
-
-				}
+			} else {
+				sm = ss
 
 			}
-			//	}
 
-			//	sort.Sort(sortstruct(pos))
-			//	for = range pos {
-			//		 uint64
+			/*
+						fmt.Println(gr, sm)
+						fmt.Println(ss)
+						if gr-sm == 1 || gr-sm == -1 {
+							sd := analyse(pos[ii], com[ss-1])
+				var kf, gf uint64
+				for g := 0; g < 4; g++ {
+					//	start := time.Now()
+							fmt.Println(sd)
+							if sd == true {
+								elem = []int{ii, ss - 1}
+								break
+							} else {
+								break
+							}
+						}
+			*/
+		}
+		//	fmt.Println(sgg-1, tcom[sgg-1], (sgg-1)%4)
+		if sgg == 0 {
+			_, pos[ii].a, pos[ii].b = analyse(pos[ii], incom, sgg-1, n)
 
-			//fmt.Fprintf(os.Stdout, "result %d , %d , %d\n", v.a, v.b, v.ran)
-		*/
-		//}
+		} else {
+			_, pos[ii].a, pos[ii].b = analyse(pos[ii], tcom[sgg-1], sgg-1, com[sgg-1][2])
+		}
+		//	fmt.Println(pos[ii])
+		//fmt.Println(elem, (elem[1]%4)+1)
+		//	elapsed := time.Since(start)
+		//	ee += elapsed
+		//if ii%1000 == 0 {
+		//	log.Println(" Binomial took ", ee.String())
+
+		//	fmt.Println(pos[ii]
+
+		var a, b uint64
+		pos[ii].angle = ((sgg - 1) % 4)
+		fmt.Println(pos[ii], sgg-1)
+		//	fmt.Println("index ", elem[0], "come ", elem[1], "angle ", elem[1]%4)
+		if sgg == 0 {
+
+			a, b = tranf(pos[ii], incom)
+		} else {
+			a, b = tranf(pos[ii], com[sgg])
+		}
+		//	fmt.Println(pos)
+
+		result[ii] = append(result[ii], a)
+		result[ii] = append(result[ii], b)
+
+		//sortstruct(pos).Swap(guid, ii)
+
 	}
+
+	//	}
+
+	//	sort.Sort(sortstruct(pos))
+	//	for = range pos {
+	//		 uint64
+
+	//fmt.Fprintf(os.Stdout, "result %d , %d , %d\n", v.a, v.b, v.ran)
+
+	//}
+
 	return result
 }
 
