e132be1b0746f9876c2a252844b12e84c08050e8
wcraft <37180689+ibrahimk9000@users.noreply.github.com>
2019-07-10 22:29:04 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 // created by ibrahim khochmane
 // email :ibra1990ski@gmail.com
-//just a messy and unmentenable code
+//just a messy and unmaintenable code
 //excution time 299.499202ms for test 4 (I5 pc)
 // O(n log n) approximate
 
